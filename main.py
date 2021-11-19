22ac36aef78715fce619fabab79695609e25da43
tirumulu123 <tirumalaraomattigunta@gmail.com>
2021-11-19 17:05:56 +0530

    apps

---
+++ main.py
@@ -0,0 +1,5 @@
+s=input()
+if s==s[::-1]:
+ print("polindrome")
+else:
+ print("not polindrome")
---
+++ main1.py
@@ -0,0 +1,7 @@
+n=int(input())
+for i in range(1.1+n//2):
+ if(n==i*i):
+  print("perfect square")
+  break
+else:
+  print("not perfect squre")
